9a2d9aae26821e0d221dc5e1019a03e378fefc55
Zhang-yizhang <1179402648@qq.com>
2026-07-15 09:28:29 +0800

    配置文件修改

--- src/config/service.js
+++ src/config/service.js
@@ -1,7 +1,6 @@
 // 天地图token
-const TDT_TOKEN = ""
+const TDT_TOKEN = "fa7ec9766b2c00747e3dd60ab3d05892"
 // 星图token
-// const GEOVISEARTH = "9729fb2df9f3e6c029be7b3aa0a73579"
 const GEOVISEARTH = "15d2ddab335383b5d485b78a3e04ac9f92683ee2e3b04cdf862cb292168674e7"
 
 export { TDT_TOKEN, GEOVISEARTH }
